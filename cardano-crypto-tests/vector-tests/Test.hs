9c0f1a83be76811fcaadf98da797a824174a1d0c
nabin6246 <nabin62246@gmail.com>
2022-10-17 19:16:45 +0545

    Add unit and vector tests for secp256k1

---
+++ cardano-crypto-tests/vector-tests/Test.hs
@@ -0,0 +1,20 @@
+
+module Test 
+where
+
+import qualified Test.EcdsaSecp256k1Tests as EcdsaSecp256k1Tests
+import qualified Test.SchnorrSecp256k1Tests as SchnorrSecp256k1Tests
+import qualified TestVector.EcdsaSecp256k1Vectors as EcdsaSecp256k1Vectors
+import qualified TestVector.SchnorrSecp256k1Vectors as SchnorrSecp256k1Vectors
+import           Test.Tasty
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Secp-256k1 tests" [
+         EcdsaSecp256k1Tests.tests,
+         SchnorrSecp256k1Tests.tests,
+         EcdsaSecp256k1Vectors.tests,
+         SchnorrSecp256k1Vectors.tests    
+    ]
---
+++ cardano-crypto-tests/vector-tests/Util/Utils.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+
+module Util.Utils
+where
+
+import Data.ByteString ( ByteString )
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base16 as BS16
+import Cardano.Binary (ToCBOR, serialize')
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.ByteString.UTF8 as BSU      -- from utf8-string
+
+-- Convert raw bytes to base16
+toHex :: ToCBOR a => a -> Int -> String
+toHex a dropFront = T.unpack $ T.decodeUtf8 $ BS.drop dropFront $ BS16.encode $ serialize' a
+
+--Convert bas16 to raw bytes
+unHex :: ByteString -> Either String ByteString
+unHex = BS16.decode
+
+-- Convert byteString to String
+byteStringToString :: ByteString -> String
+byteStringToString = T.unpack . T.decodeUtf8
+
+toHexByteString :: ByteString -> ByteString
+toHexByteString = BS16.encode
+
+convertToBytes :: String -> String -> IO ByteString
+convertToBytes prefix hexStr = do 
+    let hexBs = BSU.fromString $ prefix ++ hexStr
+    let bytesE = unHex hexBs
+    case bytesE of
+        Left _ -> error "Error: Couldn't unHex the Hex string. Incorrect format."
+        Right bytes' -> pure bytes'
