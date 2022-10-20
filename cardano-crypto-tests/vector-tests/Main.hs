250d05b16dbc83d874ab0bac77f8b16968c8e031
nabin6246 <nabin62246@gmail.com>
2022-10-20 21:25:08 +0545

    Remove csv files and not required unit tests

--- cardano-crypto-tests/vector-tests/Main.hs
+++ cardano-crypto-tests/vector-tests/Main.hs
@@ -1,7 +1,5 @@
 module Main (main) where
 
-import qualified Test.EcdsaSecp256k1Tests as EcdsaSecp256k1Tests
-import qualified Test.SchnorrSecp256k1Tests as SchnorrSecp256k1Tests
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import qualified TestVector.EcdsaSecp256k1Vectors as EcdsaSecp256k1Vectors
 import qualified TestVector.SchnorrSecp256k1Vectors as SchnorrSecp256k1Vectors
@@ -12,9 +10,7 @@ main = defaultMain tests
 tests :: TestTree
 tests =
   testGroup
-    "Secp-256k1 tests"
-    [ EcdsaSecp256k1Tests.tests,
-      SchnorrSecp256k1Tests.tests,
-      EcdsaSecp256k1Vectors.tests,
+    "Secp-256k1 Vector tests"
+    [ EcdsaSecp256k1Vectors.tests,
       SchnorrSecp256k1Vectors.tests
     ]
